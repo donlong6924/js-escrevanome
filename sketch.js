7348c4ca45ee2b3c4612fca36de155930a160f70
Pedro Lucas <pedrolucassantos00321@gmail.com>
2024-08-13 16:58:11 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,41 +1,14 @@
-let cor;
-let posicaoHorizontal; // x
-let posicaoVertical; // y
-
 function setup() {
   createCanvas(400, 400);
-   background(color(100,0,0));
-  cor = color(random(0,255), random(0,255), random(0,255));
-  posicaoHorizontal = 200;
-  posicaoVertical = 200;
+  background("black")
 }
 
-
 function draw() {
-  
-  fill(cor);
-  circle(posicaoHorizontal,posicaoVertical,50);
- 
+  stroke("red");
+  fill("white");
   
   
-  if (mouseX < posicaoHorizontal){
-    posicaoHorizontal =  posicaoHorizontal - 1;
+  if (mouseIsPressed) {
+    rect(mouseX, mouseY, 20, 35);
   }
-  
-  if (mouseX > posicaoHorizontal){
-    posicaoHorizontal =  posicaoHorizontal + 1;
-  }
-  
-  if (mouseY < posicaoVertical){
-    posicaoVertical--;
-  }
-    
-  if (mouseY > posicaoVertical){
-    posicaoVertical++;
-  }
-  
-  if (mouseIsPressed){
-    cor = color(random(0,255), random(0,255), random(0,255), random(0,100));
-  }
-    
 }
